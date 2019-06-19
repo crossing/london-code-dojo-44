eeab9d83166d834211d80225a1082089b7403e96
Deon Tan <deonjtan@gmail.com>
2019-06-20 00:05:19 +0100

    Rename graph accessors

--- src/shortest_path/core.clj
+++ src/shortest_path/core.clj
@@ -1,10 +1,10 @@
 (ns shortest-path.core)
 
-(defn nodes [graph]
+(defn get-nodes [graph]
   (dedupe (concat (keys graph)
                   (apply concat (vals graph)))))
 
-(defn neighbours [graph node]
+(defn get-neighbours [graph node]
   (let [->node key
         implied-node? (fn [[_ neighbours]] (neighbours node))
         implicit-nodes (->> graph
@@ -13,17 +13,19 @@
         explicit-nodes (graph node)]
     (into #{} (concat explicit-nodes implicit-nodes))))
 
+(defn tried-all-nodes? [path neighbours]
+  (= (set path) neighbours))
+
 (defn shortest-path [graph start end]
   (loop [path []
          start start]
-    (let [nodes (neighbours graph start)]
-      (cond (nodes end)
+    (let [neighbours (get-neighbours graph start)]
+      (cond (neighbours end)
             (conj path start end)
 
-            (= (set path)
-               nodes) ;; reached the end
+            (tried-all-nodes? path neighbours) ;; reached the end
             (conj path start)
 
             :else
             (recur (conj path start)
-                   (first nodes))))))
+                   (first neighbours))))))
--- test/shortest_path/core_test.clj
+++ test/shortest_path/core_test.clj
@@ -23,34 +23,34 @@
         (is (= [:a :b :c]
                (sut/shortest-path graph :a :c)))))))
 
-(deftest nodes
+(deftest get-nodes
   (testing "for a graph of two nodes"
     (let [graph {:a #{:b}}]
       (testing "should return the all the nodes"
         (is (= [:a :b]
-               (sut/nodes graph))))))
+               (sut/get-nodes graph))))))
   (testing "for a graph of three nodes"
       (let [graph {:a #{:b}
                    :b #{:c}}]
         (testing "should find both a and c for b"
           (is (= [:a :b :c]
-                 (sut/nodes graph)))))))
+                 (sut/get-nodes graph)))))))
 
-(deftest neighbours
+(deftest get-neighbours
   (testing "for a graph of two nodes"
     (let [graph {:a #{:b}}]
       (testing "should return the neighbours"
         (is (= #{:b}
-               (sut/neighbours graph :a))))
+               (sut/get-neighbours graph :a))))
       (testing "should find the implicit neighbours of b"
         (is (= #{:a}
-               (sut/neighbours graph :b))))))
+               (sut/get-neighbours graph :b))))))
   (testing "for a graph of three nodes"
     (let [graph {:a #{:b}
                  :b #{:c}}]
       (testing "should find both a and c for b"
         (is (= #{:a :c}
-               (sut/neighbours graph :b)))))))
+               (sut/get-neighbours graph :b)))))))
 
 ;; SPECS
 ;; shortest-path should take a graph
