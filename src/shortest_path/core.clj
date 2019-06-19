8970e4e7d04bdcf1ba87b40ce3de76d259adbbf0
Xing Yang <xor@jecity.net>
2019-06-19 20:26:35 +0100

    Nodes and neighbours

--- src/shortest_path/core.clj
+++ src/shortest_path/core.clj
@@ -1,7 +1,22 @@
 (ns shortest-path.core)
 
+#_(defn nodes [graph]
+  (set (flatten (map (fn [x] (if (set? x) (vec x))) graph))))
+
+(defn nodes [graph]
+  (let [[n neighbours] graph]
+    (concat [n] neighbours)))
+
+(defn neighbours [graph node]
+  (let [[n [neighbour & _]] graph]
+    (if (= n node)
+      [neighbour]
+      [n])))
+
 (defn shortest-path [graph start end]
   (let [[n neighbours] graph]
     (if (seq neighbours)
-      [start end]
-      [])))
+      (if (= n start)
+        (nodes graph)
+        (reverse (shortest-path graph end start)))
+      [n])))
--- test/shortest_path/core_test.clj
+++ test/shortest_path/core_test.clj
@@ -5,14 +5,35 @@
 (deftest shortest-path
   (testing "for the graph of one node"
     (let [graph [:a #{}]]
-      (testing "should produce no paths"
-        (is (= []
+      (testing "should produce the only node"
+        (is (= [:a]
                (sut/shortest-path graph :a :a))))))
   (testing "for the graph of two nodes"
     (let [graph [:a #{:b}]]
       (testing "should produce the only path"
         (is (= [:a :b]
-               (sut/shortest-path graph :a :b)))))))
+               (sut/shortest-path graph :a :b)))
+        (testing "should work backwards"
+          (is (= [:b :a]
+                 (sut/shortest-path graph :b :a))))))))
+
+(deftest nodes
+  (testing "for a graph of two nodes"
+    (let [graph [:a #{:b}]]
+      (testing "should return the all the nodes"
+        (is (= [:a :b]
+               (sut/nodes graph)))))))
+
+(deftest neighbours
+  (testing "for a graph of two nodes"
+    (let [graph [:a #{:b}]]
+      (testing "should return the neighbours"
+        (is (= [:b]
+               (sut/neighbours graph :a))))
+      (testing "should find the implicit neighbours of b"
+        (is (= [:a]
+               (sut/neighbours graph :b))))))
+  )
 
 ;; SPECS
 ;; shortest-path should take a graph
