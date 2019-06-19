fbf26d08f6d6c2b51bc86e54b6c7b7df710ddd16
Xing Yang <xor@jecity.net>
2019-06-19 19:50:44 +0100

    Graph of two nodes

--- src/shortest_path/core.clj
+++ src/shortest_path/core.clj
@@ -1,4 +1,7 @@
 (ns shortest-path.core)
 
 (defn shortest-path [graph start end]
-  [])
+  (let [[n neighbours] graph]
+    (if (seq neighbours)
+      [start end]
+      [])))
--- test/shortest_path/core_test.clj
+++ test/shortest_path/core_test.clj
@@ -7,10 +7,14 @@
     (let [graph [:a #{}]]
       (testing "should produce no paths"
         (is (= []
-               (sut/shortest-path graph :a :a)))))))
+               (sut/shortest-path graph :a :a))))))
+  (testing "for the graph of two nodes"
+    (let [graph [:a #{:b}]]
+      (testing "should produce the only path"
+        (is (= [:a :b]
+               (sut/shortest-path graph :a :b)))))))
 
 ;; SPECS
 ;; shortest-path should take a graph
 ;; should produce a list of candidate paths
-;; [:a #{}]
-;; []
+
